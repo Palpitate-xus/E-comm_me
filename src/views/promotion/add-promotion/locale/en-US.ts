1672a431e185532ebf5c143152a41ebf9ec1fc91
Palpitate <xus1758011080@outlook.com>
2023-06-28 11:31:08 +0800

    add locale

---
+++ src/views/promotion/add-promotion/locale/en-US.ts
@@ -0,0 +1,71 @@
+export default {
+  'menu.form.step': 'Step Form',
+  'promotionForm.step.title': 'Create Channel Forms',
+  'promotionForm.step.title.baseInfo': 'Select Basic Information',
+  'promotionForm.step.subTitle.baseInfo': 'Channel creation activities',
+  'promotionForm.step.title.channel': 'Channel Information',
+  'promotionForm.step.subTitle.channel': 'Select upstream of domain',
+  'promotionForm.step.title.finish': 'Finish',
+  'promotionForm.step.subTitle.finish': 'Submit success',
+  'promotionForm.success.title': 'Success',
+  'promotionForm.success.subTitle': 'The form is submitted successfully!',
+  'promotionForm.button.next': 'Next',
+  'promotionForm.button.prev': 'Prev',
+  'promotionForm.button.submit': 'Submit',
+  'promotionForm.button.again': 'Again',
+  'promotionForm.button.view': 'Detail',
+  'promotionForm.label.type.web': 'web domain',
+  'promotionForm.label.type.api': 'api domain',
+  // form -1
+  'promotionForm.form.label.activityName': 'Activity Name',
+  'promotionForm.placeholder.activityName':
+    'Enter a maximum of 20 Chinese characters, letters, or digits',
+  'promotionForm.form.error.activityName.pattern':
+    'Enter a maximum of 20 Chinese characters, letters, or digits',
+  'promotionForm.form.error.activityName.required':
+    'Please enter the activity name',
+  'promotionForm.form.label.channelType': 'Channel Type',
+  'promotionForm.placeholder.channelType': 'Select a channel type',
+  'promotionForm.form.error.channelType.required':
+    'Please select a channel type',
+  'promotionForm.form.label.promotionTime': 'Promotion Time',
+  'promotionForm.form.error.promotionTime.required':
+    'Please select the promotion time',
+  'promotionForm.form.label.promoteLink': 'Promote Link',
+  'promotionForm.form.error.promoteLink.required':
+    'Please enter the promotion link',
+  'promotionForm.form.error.promoteLink.pattern':
+    'For example, the download address of Android or iOS or the intermediate URL must start with http:// or https://',
+  'promotionForm.form.tip.promoteLink':
+    'For example, the download address of Android or iOS or the intermediate URL must start with http:// or https://',
+  'promotionForm.placeholder.promoteLink':
+    'Please enter the promotion page Link',
+
+  // form -1 end
+  // form -2
+  'promotionForm.form.label.advertisingSource': 'Advertising Source',
+  'promotionForm.placeholder.advertisingSource':
+    'Introduction source address: Sohu, Sina',
+  'promotionForm.form.error.advertisingSource.required':
+    'Please enter the advertising source',
+  'promotionForm.form.label.advertisingMedia': 'Advertising Media',
+  'promotionForm.placeholder.advertisingMedia':
+    'Marketing media: CPC, Bannner, EDM',
+  'promotionForm.form.error.advertisingMedia.required':
+    'Please enter the advertising media',
+  'promotionForm.form.label.keyword': 'keyword',
+  'promotionForm.placeholder.keyword': 'Please select keyword',
+  'promotionForm.form.error.keyword.required': 'Please select keyword',
+  'promotionForm.form.label.pushNotify': 'Push Notify',
+  'promotionForm.form.label.advertisingContent': 'Advertising Content',
+  'promotionForm.placeholder.advertisingContent':
+    'Please enter the description of advertisement content, the maximum is 200 words',
+  'promotionForm.form.error.advertisingContent.required':
+    'Please enter the description of advertisement content',
+  'promotionForm.form.error.advertisingContent.maxLength':
+    'the maximum is 200 words',
+  // form -2 end
+  'promotionForm.form.description.title': 'Channel Form Description',
+  'promotionForm.form.description.text':
+    'Advertiser channel promotion supports tracking of users who download apps by placing ads on third-party advertisers, such as toutiao channel, and tracking users who activate apps by downloading apps through channels.',
+};
